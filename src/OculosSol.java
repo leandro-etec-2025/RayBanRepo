9e6b3467fbac7cb92647fcf30fc247261cbb4f66
leandro-etec-2025 <leandrocesardacruz.cps.2025@gmail.com>
2025-11-28 10:43:39 -0300

    Adicionadas as Classes

--- src/OculosSol.java
+++ src/OculosSol.java
@@ -1,4 +1,25 @@
 
-public class OculosSol {
+public class OculosSol extends Produtos{
+	
+	String corLente;
+	
+	// Construtor
+	public OculosSol(String cor, float preco, String pigmentacao){
+		super(cor, preco);
+
+		this.corLente = pigmentacao;
+	
+	}
+	
+	public void setCorLente(String pigmento){
+		this.corLente = pigmento;
+	}
+	
+	public String getCorLente(){
+		return corLente;
+	}
+	
+	
+	
 
 }
--- src/Produtos.java
+++ src/Produtos.java
@@ -1,4 +1,29 @@
 
 public class Produtos {
+	
+	String cor;
+	float preco;
+	
+	// Construtor
+	public Produtos(String c, float p){
+		this.cor = c;
+		this.preco = p;
+	}
+
+	public float getPreco(){
+		return preco;
+	}
+
+	public String getCor(){
+		return cor;
+	}
+
+	public void setCor(String color){
+		this.cor = color;
+	}
+
+	public void setPreco(float price){
+		this.preco = price;
+	}
 
 }
